eb24f70ded65a0a9618757fcd5942bf3a94348eb
root <root@DESKTOP-HOHFE4J.localdomain>
2022-08-02 13:03:56 +0545

    Task Completed

---
+++ src/components/todo-form/todo-form.jsx
@@ -0,0 +1,44 @@
+import { useContext, useState } from "react";
+import { TodosContext } from "../../todo-context";
+import "./todo-form.scss";
+
+export const TodoForm = () => {
+  const { todos, setTodos, primaryKey, setPrimaryKey } =
+    useContext(TodosContext);
+  const [task, setTask] = useState("");
+
+  const handleAddTodo = () => {
+    // Fin an ability to add new task
+
+    console.log(todos);
+    setTodos((prevTodos) => [
+      {
+        id: primaryKey + 1,
+        label: task,
+        checked: false,
+      },
+      ...prevTodos,
+    ]);
+    setPrimaryKey(primaryKey + 1);
+  };
+
+  const handleKeyUp = (e) => {
+    if (e.keyCode === 13) {
+      handleAddTodo();
+    }
+  };
+
+  return (
+    <div className="todo-form">
+      <input
+        placeholder="Enter new task"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+        onKeyUp={handleKeyUp}
+      />
+      <button type="button" onClick={handleAddTodo}>
+        Add task
+      </button>
+    </div>
+  );
+};
